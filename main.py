a57bb2fd609ba23c5f467e075db3f1328ef33923
Arthur Pope <spartanbard@gmail.com>
2018-04-16 09:42:00 -0500

    Blog posts and input form on same page

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog: @localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'bonerfart'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(10000))
+    
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        post_title = request.form['post-title']
+        post_body = request.form['post-body']
+        blog_post = Blog(post_title, post_body)
+        db.session.add(blog_post)
+        db.session.commit()
+
+    posts = Blog.query.all()
+    return render_template('blog.html', title="Big Ballin'Blog", posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
